8e2d05b2f2b27b983f32c60fe0466c9a71a7ace3
conversion ignition--harmonic <conversion ignition--harmonic>
2026-03-26 13:58:38 +0100

    Update: modifiche locali fra2mo_description

--- launch/fra2mo_bringup.launch.py
+++ launch/fra2mo_bringup.launch.py
@@ -1,9 +1,11 @@
+
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
 
@@ -12,32 +14,26 @@ def generate_launch_description():
     xacro_file = os.path.join(fra2mo_description_dir, 'urdf', 'fra2mo.urdf.xacro')
     
     # Usa xacro per processare il file e generare la stringa XML del robot
-    robot_description_content = Command(['xacro ', xacro_file])
+    robot_description_content = ParameterValue(Command(['xacro ', xacro_file]), value_type=str)
     
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description_content, 'use_sim_time': False}] 
+        parameters=[{'robot_description': robot_description_content, 'use_sim_time': False}]
         # NOTA: use_sim_time è False perché ora usiamo il tempo reale del Raspberry!
     )
 
-    # 2. Configurazione per il Micro-ROS Agent (Comunica con il Pico via USB)
-    # Assumiamo che il Pico sia collegato alla porta /dev/ttyACM0
-    micro_ros_agent_node = Node(
-        package='micro_ros_agent',
-        executable='micro_ros_agent',
-        name='micro_ros_agent',
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
         output='screen',
-        arguments=['serial', '--dev', '/dev/ttyACM0', '-v6'] 
-        # -v6 stampa log dettagliati, utile per il debug iniziale
+        parameters=[{'use_sim_time': False}]
     )
 
-    # 3. (Futuro) Nodo del LiDAR reale
-    # Lo aggiungeremo qui non appena mi dirai che modello di LiDAR possiedi!
-
     return LaunchDescription([
-        robot_state_publisher_node,
-        micro_ros_agent_node
+        joint_state_publisher_node,
+        robot_state_publisher_node
     ])
