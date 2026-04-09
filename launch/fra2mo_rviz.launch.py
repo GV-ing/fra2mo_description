4674d5efa3d4e97d04cbcdbe0204ec6e0d4e2456
conversion ignition--harmonic <conversion ignition--harmonic>
2026-04-09 13:53:56 +0200

    Update local changes

--- launch/fra2mo_rviz.launch.py
+++ launch/fra2mo_rviz.launch.py
@@ -76,7 +76,7 @@ def generate_launch_description():
     return LaunchDescription([
         #robot_state_publisher_node,
         #joint_state_publisher_node,
-        telop_node,
-        joy_node,
+        #telop_node,
+        #joy_node,
         rviz_node
     ])
